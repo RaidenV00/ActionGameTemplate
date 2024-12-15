61164e55fe170a789a8e57179f347e2a62fcbb57
RaidenV_00 <RaidenV_00@outlook.com>
2024-12-16 00:55:41 +0800

    升级框架

--- Plugins/WindowsGamingInput/Intermediate/Build/Win64/UnrealEditor/Inc/WindowsGamingInput/UHT/WindowsGamingInput.init.gen.cpp
+++ Plugins/WindowsGamingInput/Intermediate/Build/Win64/UnrealEditor/Inc/WindowsGamingInput/UHT/WindowsGamingInput.init.gen.cpp
@@ -17,7 +17,7 @@ void EmptyLinkFunctionForGeneratedCodeWindowsGamingInput_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x1C92DEA7,
+				0xC20BD213,
 				0xCF123F25,
 				METADATA_PARAMS(0, nullptr)
 			};
@@ -25,5 +25,5 @@ void EmptyLinkFunctionForGeneratedCodeWindowsGamingInput_init() {}
 		}
 		return Z_Registration_Info_UPackage__Script_WindowsGamingInput.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_WindowsGamingInput(Z_Construct_UPackage__Script_WindowsGamingInput, TEXT("/Script/WindowsGamingInput"), Z_Registration_Info_UPackage__Script_WindowsGamingInput, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x1C92DEA7, 0xCF123F25));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_WindowsGamingInput(Z_Construct_UPackage__Script_WindowsGamingInput, TEXT("/Script/WindowsGamingInput"), Z_Registration_Info_UPackage__Script_WindowsGamingInput, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xC20BD213, 0xCF123F25));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
